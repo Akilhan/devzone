11d07c017ec6476948cb09e2cf32ffe826e37681
Akilhan <oqiljondadaxanov@gmail.com>
2024-10-02 18:27:49 +0500

    folder structure fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import DevZoneLanding from "./components/landingpage";
+import DevZoneLanding from "../components/landingpage";
 
 export default function Home() {
   return (
-
-
     <div>
       <DevZoneLanding />
     </div>
--- components/landingpage.tsx
+++ components/landingpage.tsx
@@ -165,7 +165,7 @@ export default function DevZoneLanding() {
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-zinc-500 dark:text-zinc-400">© 2024 DevZone. All rights reserved.</p>
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">© 2024 DevZone & akilhan. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Terms of Service
