b081643cbb30f50849964627f6629d38315f5d1d
Okiljon <96512610+Akilhan@users.noreply.github.com>
2024-10-02 20:18:43 +0500

    Update layout.tsx
    updated google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XVDBGKJFN2');
+              gtag('config', 'G-5N3H3YSJ1W');
             `,
           }}
         />
